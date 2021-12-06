14b2fa8720e289f30a66cac3b78cd6bd11a51ed0
ALBATOR <SST.plomberie@laposte.net>
2021-12-06 20:38:19 +0100

    suppression

--- index.js
+++
@@ -1,5 +0,0 @@
-if ("serviceWorker" in navigator) {
-    //tu me mets en place un service Worker
-    navigator.serviceWorker.register("/serviceWorker.js").then(()
-        => console.log("sw registered")).catch((err) => console.log(err));
-}
